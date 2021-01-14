629cbeac1d356c951484a912a53070b0ee2150c9
Trung Hoang <hoang.n.trung171@gmail.com>
2021-01-14 16:13:31 +0700

    part 17

--- app.js
+++ app.js
@@ -129,6 +129,53 @@ const mapStyle = [
       styles: mapStyle,
     });
   
+    const input = document.getElementById("pac-input");
+    const searchBox = new google.maps.places.SearchBox(input);
+    // Bias the SearchBox results towards current map's viewport.
+    map.addListener("bounds_changed", () => {
+      searchBox.setBounds(map.getBounds());
+    });
+    let markers = [];
+    // Listen for the event fired when the user selects a prediction and retrieve
+    // more details for that place.
+    searchBox.addListener("places_changed", () => {
+      const places = searchBox.getPlaces();
+
+      if (places.length == 0) {
+        return;
+      }
+      // Clear out the old markers.
+      markers.forEach((marker) => {
+        marker.setMap(null);
+      });
+      markers = [];
+      // For each place, get the icon, name and location.
+      const bounds = new google.maps.LatLngBounds();
+      places.forEach((place) => {
+        if (!place.geometry) {
+          console.log("Returned place contains no geometry");
+          return;
+        }
+
+        // Create a marker for each place.
+        markers.push(
+          new google.maps.Marker({
+            map,
+            title: place.name,
+            position: place.geometry.location,
+          })
+        );
+
+        if (place.geometry.viewport) {
+          // Only geocodes have viewport.
+          bounds.union(place.geometry.viewport);
+        } else {
+          bounds.extend(place.geometry.location);
+        }
+      });
+      map.fitBounds(bounds);
+    });
+
     if (navigator.geolocation) {
       navigator.geolocation.getCurrentPosition(showPosition);
     } else {
@@ -191,7 +238,6 @@ const mapStyle = [
     const titleBar = document.createElement('div');
     const title = document.createElement('div');
     const container = document.createElement('div');
-    const input = document.createElement('input');
     const options = {
       types: [],
     };
@@ -199,9 +245,6 @@ const mapStyle = [
     title.textContent = 'Find a store';
     titleBar.appendChild(title);
     container.setAttribute('id', 'pac-container');
-    input.setAttribute('id', 'pac-input');
-    input.setAttribute('type', 'text');
-    input.setAttribute('placeholder', 'Find a store');
     container.appendChild(input);
     card.appendChild(titleBar);
     card.appendChild(container);
@@ -210,9 +253,6 @@ const mapStyle = [
     // which detail fields should be returned about the place that
     // the user selects from the suggestions.
     const autocomplete = new google.maps.places.Autocomplete(input, options);
-  
-    autocomplete.setFields(
-        ['address_components', 'geometry', 'name']);
   
     // Set the origin point when the user selects an address
     const originMarker = new google.maps.Marker({map: map});
@@ -227,7 +267,6 @@ const mapStyle = [
       if (!place.geometry) {
         // User entered the name of a Place that was not suggested and
         // pressed the Enter key, or the Place Details request failed.
-        window.alert('No address available for input: \'' + place.name + '\'');
         return;
       }
   
@@ -419,7 +458,6 @@ const mapStyle = [
     distancesList.sort((first, second) => {
       return first.distanceVal - second.distanceVal;
     });
-  
     return distancesList;
   }
   
